5cb605675ef4f2ab8a8e91e6f2b4cf6a3c6ef57e
nataly <15marzonataly@gmail.com>
2023-10-28 16:48:58 +0200

    Añadir ejercicio 01

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,27 @@
+const nombre = 'Nataly'
+
+const edad = 33
+
+const esEstudiante = true
+
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy esEstudiante: ' + esEstudiante + '.'
+
+const num1 = 12
+
+const num2 = 6
+
+console.log('Mensaje:', mensaje)
+
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
+
+const frase = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy esEstudiante: ' + esEstudiante + '.'
+
+console.log(mensaje)
+console.log('Suma:', suma)
+console.log('Resta:', resta)
+console.log('Multiplicación:', multiplicacion)
+console.log('División:', division)
+console.log(frase)
